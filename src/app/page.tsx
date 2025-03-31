cd131f99549ad67c1e0006aceed90504b7ef8355
LCR <cariou.liam@orange.fr>
2025-03-31 11:49:06 +0200

    ajout de l'img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1 className="text-2xl">Bienvenue sur accueil</h1>;
+  return (
+    <div>
+      <h1 className="text-2xl mb-4">Bienvenue sur accueil</h1>
+      <Image
+        src="/img.jpg"
+        alt="img"
+        width={800}
+        height={600}
+        priority
+        className="rounded shadow-lg"
+      />
+    </div>
+  );
 }
