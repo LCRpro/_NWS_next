417e5f7d7b699387b02d9eaa312f47f710165754
LCR <cariou.liam@orange.fr>
2025-03-31 11:46:33 +0200

    3 pages

---
+++ src/app/a-propos/page.tsx
@@ -0,0 +1,4 @@
+export default function APropos() {
+    return <h1 className="text-2xl">Bienvenue sur A propos</h1>;
+  }
+  
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,4 @@
+export default function Contact() {
+    return <h1 className="text-2xl">Bienvenue sur contact</h1>;
+  }
+  
